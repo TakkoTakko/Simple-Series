53d1e674de0fb6ae9c4360ef5bef16b4964e5cfb
TakkoTakko <tjarklit@gmail.com>
2021-11-10 21:43:41 +0100

    added join & quit message module

---
+++ Join_Quit_Messages/src/main/java/de/takko/simple/module/join_quit_message/JQListener.java
@@ -0,0 +1,23 @@
+package de.takko.simple.module.join_quit_message;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class JQListener implements Listener {
+
+    @EventHandler
+    public void handle(PlayerJoinEvent event) {
+        if (Boolean.parseBoolean(Join_Quit_MessageModule.getFileManager().get("join.enabled"))) {
+            event.setJoinMessage(Join_Quit_MessageModule.getFileManager().get("join.message").replaceAll("%prefix%", Join_Quit_MessageModule.getFileManager().get("Prefix")).replaceAll("%player%", event.getPlayer().getName()));
+        }
+    }
+
+    @EventHandler
+    public void handle(PlayerQuitEvent event) {
+        if (Boolean.parseBoolean(Join_Quit_MessageModule.getFileManager().get("quit.enabled"))) {
+            event.setQuitMessage(Join_Quit_MessageModule.getFileManager().get("quit.message").replaceAll("%prefix%", Join_Quit_MessageModule.getFileManager().get("Prefix")).replaceAll("%player%", event.getPlayer().getName()));
+        }
+    }
+}
